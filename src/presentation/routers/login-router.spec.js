18e4f3163022f161433e3d00b19a2c68f810b163
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-08-21 09:02:42 -0300

    ensure LoginRouter return a valid token when valid credentials are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -85,7 +85,7 @@ describe('Login Router', () => {
   })
 
   it('Should return 200 when valid credentials are provided', () => {
-    const { sut } = makeSut()
+    const { sut, authUseCaseSpy } = makeSut()
     const httpRequest = {
       body: {
         email: 'valid_email@mail.com',
@@ -94,6 +94,7 @@ describe('Login Router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body.accessToken).toEqual(authUseCaseSpy.accessToken)
   })
 
   it('Should return 500 if AuthUseCase is not provided', () => {
