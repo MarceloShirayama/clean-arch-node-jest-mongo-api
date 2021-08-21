e70e19b669b6795ea49fbcffa1af87d4c700f92d
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-08-21 13:59:18 -0300

    ensure EmailValidator call validator with correct email

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -23,4 +23,11 @@ describe('EmailValidator', () => {
     const isEmailValid = sut.isValid('invalid_email@mail.com')
     expect(isEmailValid).toBe(false)
   })
+
+  it('Should call validator with correct email', () => {
+    validator.isEmailValid = false
+    const sut = makeSut()
+    sut.isValid('any_email@mail.com')
+    expect(validator.email).toBe('any_email@mail.com')
+  })
 })
