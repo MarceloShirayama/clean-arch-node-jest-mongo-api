d24d4941a70daa98169045abd746f888f35ad8b3
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-08-24 18:17:26 -0300

    ensure LoadUserByEmailRepository return user if user is found

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'jest-environment-node',
-  collectCoverageFrom: ['**/src/**/*.js']
+  collectCoverageFrom: ['**/src/**/*.js'],
+  preset: '@shelf/jest-mongodb',
+  watchPathIgnorePatterns: ['globalConfig']
 }
