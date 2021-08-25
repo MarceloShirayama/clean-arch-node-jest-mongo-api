67d9c39d9c149e98a9f6eb7d0e76c64c9d3d5b1d
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-08-25 19:02:31 -0300

    disable x-powered-byin Request Headers

---
+++ src/main/config/setup.test.js
@@ -0,0 +1,13 @@
+const supertest = require('supertest')
+const app = require('./app')
+const request = supertest(app)
+describe('App Setup', () => {
+  it('Should disable x-powered-by header', async () => {
+    app.get('/test_x_powered_by', (req, res) => {
+      res.send('teste')
+    })
+
+    const res = await request.get('/test_x_powered_by')
+    expect(res.headers['x-powered-by']).toBeUndefined()
+  })
+})
--- src/main/index.js
+++ src/main/index.js
@@ -1,12 +1,8 @@
-const express = require('express')
-
-const app = express()
+const app = require('./config/app')
 
 const host = 'localhost'
 const port = 5858
 const url = `http://${host}:${port}`
-
-app.disable('x-powered-by')
 
 app.get('/api/mango', (req, resp) => {
   resp.send('Curso de NodeJS com Clean Architecture e TDD do Mango')
