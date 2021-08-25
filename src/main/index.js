eca1293c7ee7bda36059d615f1404676b90c7597
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-08-25 17:48:41 -0300

    disable x-powered-byin Request Headers

--- src/main/index.js
+++ src/main/index.js
@@ -6,4 +6,10 @@ const host = 'localhost'
 const port = 5858
 const url = `http://${host}:${port}`
 
+app.disable('x-powered-by')
+
+app.get('/api/mango', (req, resp) => {
+  resp.send('Curso de NodeJS com Clean Architecture e TDD do Mango')
+})
+
 app.listen(port, host, () => console.log(`Server on: ${url}`))
