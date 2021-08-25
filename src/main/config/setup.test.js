5c371a695e9eae4cc4a96cabf271f347319d508c
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-08-25 19:30:43 -0300

    move test cors to its own file

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -11,13 +11,4 @@ describe('App Setup', () => {
     const res = await request.get('/test_x_powered_by')
     expect(res.headers['x-powered-by']).toBeUndefined()
   })
-
-  it('Should enable CORS', async () => {
-    app.get('/test_cors', (req, res) => res.send(''))
-
-    const res = await request.get('/test_cors')
-    expect(res.headers['access-control-allow-origin']).toBe('*')
-    expect(res.headers['access-control-allow-methods']).toBe('*')
-    expect(res.headers['access-control-allow-headers']).toBe('*')
-  })
 })
