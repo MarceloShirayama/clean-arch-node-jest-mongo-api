776d5d16ba5c44d9eefd13f5e48541c6ed93e65d
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-08-25 19:24:20 -0300

    enable CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -1,13 +1,23 @@
 const supertest = require('supertest')
+
 const app = require('./app')
+
 const request = supertest(app)
+
 describe('App Setup', () => {
   it('Should disable x-powered-by header', async () => {
-    app.get('/test_x_powered_by', (req, res) => {
-      res.send('teste')
-    })
+    app.get('/test_x_powered_by', (req, res) => res.send(''))
 
     const res = await request.get('/test_x_powered_by')
     expect(res.headers['x-powered-by']).toBeUndefined()
+  })
+
+  it('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => res.send(''))
+
+    const res = await request.get('/test_cors')
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
   })
 })
