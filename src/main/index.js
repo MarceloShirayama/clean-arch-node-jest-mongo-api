f587666db0ea3767f4f336b7b5168ae990a0bde8
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-08-26 15:19:59 -0300

    connect to mongodb before listen for app

--- src/main/index.js
+++ src/main/index.js
@@ -1,11 +1,13 @@
-const app = require('./config/app')
+const MongoHelper = require('../infra/helpers/mongo-helper')
+const env = require('./config/env')
 
-const host = 'localhost'
-const port = 5858
-const url = `http://${host}:${port}`
-
-app.get('/api/mango', (req, resp) => {
-  resp.send('Curso de NodeJS com Clean Architecture e TDD do Mango')
-})
-
-app.listen(port, host, () => console.log(`Server on: ${url}`))
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    const app = require('./config/app')
+    const { serverPort, serverHost } = env
+    const serverUrl = `http://${serverHost}:${serverPort}`
+    app.listen(serverPort, serverHost, () => (
+      console.log(`listen on: ${serverUrl}`
+      )))
+  })
+  .catch(console.error)
